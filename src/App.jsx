8f5987b7fb1e16a5ef3e81809e4fe87eebd426dc
jiduslove <oondayr@gamil.com>
2023-04-05 20:58:15 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { createContext, useState } from "react";
+import Child from "./components/Child";
+import ChildFriend from "./components/ChildFriend";
+
+export const AppContext = createContext();
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  const [gift, setGift] = useState(10000000);
+
+  return (
+    <AppContext.Provider value={{ gift, setGift }}>
+      <div className="bg-red-100">
+        <Child />
+        <ChildFriend />
+      </div>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
